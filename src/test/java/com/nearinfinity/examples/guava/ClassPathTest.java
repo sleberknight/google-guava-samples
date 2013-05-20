821a63e0febfab1731c66906da73b8506a6f9535
Scott Leberknight <scott.leberknight@nearinfinity.com>
2013-05-19 23:56:25 -0400

    More tests of ClassPath: getResources() and getTopLevelClasses().

--- src/test/java/com/nearinfinity/examples/guava/ClassPathTest.java
+++ src/test/java/com/nearinfinity/examples/guava/ClassPathTest.java
@@ -2,6 +2,7 @@ package com.nearinfinity.examples.guava;
 
 import com.google.common.base.CharMatcher;
 import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.reflect.ClassPath;
@@ -11,6 +12,8 @@ import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.Collection;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class ClassPathTest {
@@ -32,5 +35,30 @@ public class ClassPathTest {
 
         assertTrue(classNames.contains(Function.class.getSimpleName()));
         assertTrue(classNames.contains(CharMatcher.class.getSimpleName()));
+    }
+
+    @Test
+    public void testGetResources() throws IOException {
+        ClassPath classPath = ClassPath.from(Thread.currentThread().getContextClassLoader());
+        ImmutableSet<ClassPath.ResourceInfo> resources = classPath.getResources();
+        assertThat(resources.contains("some-resource.txt"), is(true));
+    }
+
+    @Test
+    public void testGetTopLevelClasses_InSpecificPackage() throws IOException {
+        ClassPath classPath = ClassPath.from(Thread.currentThread().getContextClassLoader());
+
+        ImmutableSet<ClassPath.ClassInfo> topLevelClasses =
+                classPath.getTopLevelClasses(HashingExample.class.getPackage().getName());
+        Collection<ClassPath.ClassInfo> filtered = Collections2.filter(topLevelClasses,
+                new Predicate<ClassPath.ClassInfo>() {
+                    @Override
+                    public boolean apply(@Nullable ClassPath.ClassInfo input) {
+                        return input.getName().equals(HashingExample.class.getName());
+                    }
+                });
+        assertThat(filtered.size(), is(1));
+        ClassPath.ClassInfo classInfo = filtered.iterator().next();
+        assertThat(classInfo.getName(), is(HashingExample.class.getName()));
     }
 }
