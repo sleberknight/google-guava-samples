540357397414b1e25ee35d69ad9ea2491f0434f2
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2021-03-31 17:44:56 -0400

    Fix code in CharMatcherTest
    * Fix deprecated and removed DIGITS constant in CharMatcherTest
    * Remove guava-samples.iml (it should be ignored)

--- src/test/java/com/nearinfinity/examples/guava/CharMatcherTest.java
+++ src/test/java/com/nearinfinity/examples/guava/CharMatcherTest.java
@@ -1,40 +1,40 @@
 package com.nearinfinity.examples.guava;
 
-import com.google.common.base.CharMatcher;
-import org.junit.Test;
-
 import static com.google.common.base.CharMatcher.inRange;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import com.google.common.base.CharMatcher;
+import org.junit.Test;
+
 public class CharMatcherTest {
 
     @Test
     public void testRemoveAllDigits() {
         String input = "hello2world";
-        String result = CharMatcher.DIGIT.removeFrom(input);
+        String result = CharMatcher.inRange('0', '9').removeFrom(input);
         assertThat(result, is("helloworld"));
     }
 
     @Test
     public void testRetainAllDigits() {
         String secretCode = "this1is2a3really4good5code";
-        String result = CharMatcher.DIGIT.retainFrom(secretCode);
+        String result = CharMatcher.inRange('0', '9').retainFrom(secretCode);
         assertThat(result, is("12345"));
     }
 
     @Test
     public void testMatchingDigitsOrAlphabeticCharacters() {
-        CharMatcher matcher = CharMatcher.DIGIT.or(inRange('a', 'z').or(inRange('A', 'Z')));
+        CharMatcher matcher = CharMatcher.inRange('0', '9').or(inRange('a', 'z').or(inRange('A', 'Z')));
         assertTrue(matcher.matchesAllOf("this1will2match3"));
         assertFalse(matcher.matchesAllOf("this-will-not-match"));
     }
 
     @Test
     public void testMatchingPartOfAString() {
-        CharMatcher matcher = CharMatcher.DIGIT.or(inRange('a', 'z').or(inRange('A', 'Z'))).or(CharMatcher.is('*'));
+        CharMatcher matcher = CharMatcher.inRange('0', '9').or(inRange('a', 'z').or(inRange('A', 'Z'))).or(CharMatcher.is('*'));
         assertTrue(matcher.matchesAnyOf("@#$@hello@#$@"));
         assertFalse(matcher.matchesAnyOf("%#$##$#@@"));
     }
@@ -42,7 +42,7 @@ public class CharMatcherTest {
     @Test
     public void testStrippingWhitespace() {
         String input = "  secret passcode  ";
-        String result = CharMatcher.WHITESPACE.trimFrom(input);
+        String result = CharMatcher.whitespace().trimFrom(input);
         assertThat(result, is("secret passcode"));
     }
 }
